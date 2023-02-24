156266e74542a37cb28ccd12ffd8f90fe1b06c85
Rodrigo Andre Decuir Fuentes <ojo@ciencias.unam.mx>
2023-02-24 06:06:06 -0600

    feat: save point 06:00

--- practicas/01-practica/src/Espectador.java
+++ practicas/01-practica/src/Espectador.java
@@ -7,26 +7,51 @@ import java.io*;
 public class Espectador() {
     private String id_Espectador;
     private String apuesta;
-    private Escenario castilloDePeach ;
+    private Escenario verEscenario;
+    private String historia;
+    private String movimiento;
 
+    /**
+     * Constructor para inicializar las variables.
+     */ 
     public Espectador(String id_Espectador, String apuesta, Escenario castilloDePeach) {
         this.id_Espectador = id_Espectador;
         this.apuesta = apuesta;
         this.castilloDePeach = castilloDePeach;
     }
     /**
-     *
+     * Metodo para  
      */
     @Override
     public void actualizar() {
      
     }
-
-    public String getId_Espectador() {
+    
+    /**
+     *
+     */
+    public String obtenerId_Espectador() {
         return id_Espectador; 
     }
 
-    public String getApuesta() {
+    /**
+     *
+     */
+    public String obtenerApuesta() {
         return apuesta; 
+    }
+    
+    /**
+     *
+     */
+    public String obtenerHistoria() {
+        return historia;
+    }
+    
+    /**
+     * 
+     */
+    public void dejarDeVerEscenario() {
+     
     }
 }
--- practicas/01-practica/src/SuperSmashBrus.java
+++ practicas/01-practica/src/SuperSmashBrus.java
@@ -1,10 +1,14 @@
 /**
- * Clase para 
+ * Simulacion de un combate entre tres personajes
+ * implementando patrones Strategy y Observer.
  * @author tukuwanaminaku
  */
 public class SuperSmashBrus() {
-    public static void main(String[] args) {
+    public void ...
+    public void ...
 
+    public static void main(String[] args) {
+        
     }
 } 
     

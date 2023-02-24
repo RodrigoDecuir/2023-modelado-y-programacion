7f3feb3a79399efd0f5713c99b2be92bb770bd80
Rodrigo Andre Decuir Fuentes <ojo@ciencias.unam.mx>
2023-02-23 20:57:45 -0600

    feat: added interfaz sujeto

--- practicas/01-practica/src/Sujeto.java
+++ practicas/01-practica/src/Sujeto.java
@@ -1,11 +1,13 @@
 /**
- * Clase que
+ * Interfaz que contiene la 
+ * infraestructura de suscripcion.
  *
  * @author tukuwanaminaku
  *
  */
 
 public interface Sujeto() {
-
-
+    public void registrar(Observador b);
+    public void remover(Observador b);
+    public void notificar();
 }

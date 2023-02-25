093e16b13f3b9302e4d749167f8b4d20f61a6823
Rodrigo Andre Decuir Fuentes <ojo@ciencias.unam.mx>
2023-02-24 22:06:24 -0600

    template for skill character

---
+++ practicas/01-practica/src/strategy/Dittuu.java
@@ -0,0 +1,11 @@
+public class Dittuu {
+    private String nombreAtaque;
+    private Integer puntosAtaque;
+    private DittuuAtaque dittuu;
+
+    public Ataque( );
+    public Ataque(nombreAtaque: String, puntosAtaque: Integer);
+    public void atacar(Personaje enemigo);
+    public String tipo( );
+    public String toString( ); 
+}

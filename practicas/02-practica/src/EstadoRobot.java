4908874ef9667e87c26e231abe18a1874585e0d9
Rodrigo Andre Decuir Fuentes <ojo@ciencias.unam.mx>
2023-03-03 10:58:10 -0600

    feat: added state pattern interface for possible robot states

---
+++ practicas/02-practica/src/EstadoRobot.java
@@ -0,0 +1,12 @@
+/**
+ * Interfaz de los estados del robot.
+ */
+public interface EstadoRobot {
+    public void activar;
+    public void caminar;
+    public void mostrarMenu;
+    public void tomarOrden;
+    public void prepararOrden;
+    public void entregarOrden; 
+    public void suspender;
+} 
